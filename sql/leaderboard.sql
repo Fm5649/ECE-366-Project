440366338f16ef33d29445522fe41e766059719e
Darren Chau <darren.chau@cooper.edu>
2023-03-03 18:39:40 -0500

    Update leaderboard.sql

--- sql/leaderboard.sql
+++ sql/leaderboard.sql
@@ -1,12 +1,12 @@
 --sql file for leaderboard entries
 
 CREATE TABLE leaderboard(
-  leaderboard_rank int default 0,
+  leaderboard_rank int DEFAULT 0,
   leaderboard_username varchar(30) NOT NULL.
-  leaderboard_wins int default 0,
-  leaderboard_losses int default 0,
-  leaderboard_total_games int default 0,
-  leaderboard_elo int default 0,
+  leaderboard_wins int DEFAULT 0,
+  leaderboard_losses int DEFAULT 0,
+  leaderboard_total_games int DEFAULT 0,
+  leaderboard_elo int DEFAULT 0,
   PRIMARY KEY (leaderboard_username),
   FOREIGN KEY (leaderboard_username) REFERENCES user(user_name) ON DELETE CASCADE,
   FOREIGN KEY (leaderboard_wins) REFERENCES user(total_wins) ON DELETE CASCADE,
