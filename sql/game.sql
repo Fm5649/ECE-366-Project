4a882260bb23fb54e15f362d8091c31e9fd089ce
PawelMilewski1 <pawel.milewski@cooper.edu>
2023-03-03 20:52:11 -0500

    updated sql files

--- sql/game.sql
+++ sql/game.sql
@@ -11,15 +11,15 @@ Variable for total points: 10 total
 CREATE SEQUENCE game_seq start with 1;
 
 CREATE TABLE game(
-  game_id bigint NOT NULL DEFAULT nextval('user_seq'),
+  game_id bigint NOT NULL DEFAULT nextval('game_seq'),
   p1_id bigint NOT NULL,
   p2_id bigint NOT NULL,
   total_rounds int NOT NULL,
   current_round int NOT NULL,
   winner_id bigint DEFAULT NULL,
   PRIMARY KEY (game_id),
-  FOREIGN KEY (p1_id) REFERENCES user(user_id) ON DELETE CASCADE,
-  FOREIGN KEY (p2_id) REFERENCES user(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (p1_id) REFERENCES player(user_id) ON DELETE CASCADE,
+  FOREIGN KEY (p2_id) REFERENCES player(user_id) ON DELETE CASCADE,
   CHECK (p1_id <> p2_id),
   CHECK (winner_id = p1_id OR winner_id = p2_id)
 );  
--- sql/leaderboard.sql
+++ sql/leaderboard.sql
@@ -2,15 +2,15 @@
 
 CREATE TABLE leaderboard(
   leaderboard_rank int DEFAULT 0,
-  leaderboard_username varchar(30) NOT NULL.
+  leaderboard_username varchar(30) NOT NULL,
   leaderboard_wins int DEFAULT 0,
   leaderboard_losses int DEFAULT 0,
   leaderboard_total_games int DEFAULT 0,
   leaderboard_elo int DEFAULT 0,
   PRIMARY KEY (leaderboard_username),
-  FOREIGN KEY (leaderboard_username) REFERENCES user(user_name) ON DELETE CASCADE,
-  FOREIGN KEY (leaderboard_wins) REFERENCES user(total_wins) ON DELETE CASCADE,
-  FOREIGN KEY (leaderboard_losses) REFERENCES user(total_losses) ON DELETE CASCADE,
-  FOREIGN KEY (leaderboard_total_games) REFERENCES user(total_games) ON DELETE CASCADE,
-  FOREIGN KEY (leaderboard_elo) REFERENCES user(user_elo) ON DELETE CASCADE
+  FOREIGN KEY (leaderboard_username) REFERENCES player(player_name) ON DELETE CASCADE,
+  FOREIGN KEY (leaderboard_wins) REFERENCES player(total_wins) ON DELETE CASCADE,
+  FOREIGN KEY (leaderboard_losses) REFERENCES player(total_losses) ON DELETE CASCADE,
+  FOREIGN KEY (leaderboard_total_games) REFERENCES player(total_games) ON DELETE CASCADE,
+  FOREIGN KEY (leaderboard_elo) REFERENCES player(player_elo) ON DELETE CASCADE
 );
