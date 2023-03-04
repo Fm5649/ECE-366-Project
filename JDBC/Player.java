48681955cfe5e22b9c5b639b02c4aaffed64be0f
Darren Chau <darren.chau@cooper.edu>
2023-03-04 17:02:29 -0500

    Update and rename player.java to Player.java

---
+++ JDBC/Player.java
@@ -0,0 +1,16 @@
+
+
+package //placeholder;
+
+import //placeholder.util.DataTransferObject;
+  
+public class Player implements DataTransferObject{
+  
+  private long playerId;
+  private String userName;
+  private String passWord;
+  private int totalGames;
+  private int totalLosses;
+  private int totalWins;
+  private int ELO;
+}
