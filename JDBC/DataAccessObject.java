9154e28db9de5674d86703f8208f43005842f1c5
Darren Chau <darren.chau@cooper.edu>
2023-03-04 16:47:55 -0500

    Update DataAccessObject.java

--- JDBC/DataAccessObject.java
+++ JDBC/DataAccessObject.java
@@ -1,1 +1,20 @@
 
+//placeholder, insert actual value
+package //com.chrishong.lil.jdbc.util;
+
+import java.sql.*;
+import java.util.List;
+
+public abstract class DataAccessObject  {
+
+    protected final Connection connection;
+    protected final static String LAST_VAL = "SELECT last_value FROM ";
+    protected final static String PLAYER_SEQUENCE = "player_seq";
+
+    public DataAccessObject(Connection connection){
+        super();
+        this.connection = connection;
+    }
+
+    public abstract T findById(long id);
+}
