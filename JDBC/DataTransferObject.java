f932054e8f0824d6d94e6d114c18677b0862c919
Darren Chau <darren.chau@cooper.edu>
2023-03-04 13:11:10 -0500

    Update DataTransferObject.java

--- JDBC/DataTransferObject.java
+++ JDBC/DataTransferObject.java
@@ -1,1 +1,10 @@
+//Java file for data transfer object
 
+
+//Placeholder, insert actual folder
+package //com.chrishong.lil.jdbc.util;
+
+public interface DataTransferObject {
+
+    long getId();
+}
