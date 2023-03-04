b1c59c213aca0065dcbc501c4a8d7aa8e3e60a11
freepina <freepina@gmail.com>
2023-03-04 18:39:35 -0500

    Update Leaderboard.java

--- JDBC/Leaderboard.java
+++ JDBC/Leaderboard.java
@@ -5,4 +5,83 @@ import com.chopsticks.lil.jdbc.util.DataTransferObject;
   
 public class Leaderboard implements DataTransferObject
 {
+  private long userId;
+  private int rank;
+  private String username;
+  private int wins;
+  private int losses;
+  private int totalGames;
+  private int elo;
+  
+  public long getRank() 
+  {
+    return rank;
+  }
+  public void setRank(int rank)
+  {
+    this.rank = rank;
+  }
+
+  public String getUsername() 
+  {
+    return username;
+  }
+  public long getUserId()
+  {
+    return userId;
+  }
+  public void setUsername(String username)
+  {
+    this.username = username;
+  }
+  
+  public int getWins() 
+  {
+    return wins;
+  }
+  public void setWins(int wins)
+  {
+    this.wins = wins;
+  }
+  
+  public int getLosses() 
+  {
+    return losses;
+  }
+  public void setLosses(int losses)
+  {
+    this.losses = losses;
+  }
+  
+  public int getTotalGames() 
+  {
+    return totalGames;
+  }
+  public void setTotalGames(String totalGames)
+  {
+    this.totalGames = totalGames;
+  }
+  
+  public int getElo() 
+  {
+    return elo;
+  }
+  public void setElo(int elo)
+  {
+    this.elo = elo;
+  }
+  
+  @Override
+  public String toString() 
+  {
+      return "Leaderboard{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", rank='" + rank + '\'' +
+                ", wins='" + wins + '\'' +
+                ", losses='" + losses + '\'' +
+                ", totalGames='" + totalGames + '\'' +
+                ", elo='" + elo + '\'' +
+                '}';
+  }
 }
