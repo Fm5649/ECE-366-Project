412197cbafa62f36b6f7442a99331eaacd4bc3c1
Darren Chau <darren.chau@cooper.edu>
2023-03-04 21:25:29 -0500

    Update game_round.sql

--- sql/game_round.sql
+++ sql/game_round.sql
@@ -5,11 +5,14 @@ CREATE TABLE game_round (
     round_number int NOT NULL,
     turn_player_name varchar(30) NOT NULL, --Which users' turn it is
     player_choice varchar (50) NOT NULL, --Attack or transfer
+    
+    player_hand_used varchar(50) NOT NULL, 
+    
     player_target varchar(50) NOT NULL, --"P1Hand1, P1Hand1, P2Hand1, P2Hand2" 
     --p1_choice varchar(50) NOT NULL,
     --p2_choice varchar(50) NOT NULL,
     
-    player_action_amount int NOT NULL DEFAULT 0,  --Amount the player transfers or attacks with
+    player_action_amount int NOT NULL DEFAULT 0,  --Amount the player transfers with
     p1_hand1 int NOT NULL DEFAULT 1,    
     p1_hand2 int NOT NULL DEFAULT 1,    
     p2_hand1 int NOT NULL DEFAULT 1,
