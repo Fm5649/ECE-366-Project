4a9fa9e88354dc13225dc16e7c822097c77edaf0
Darren Chau <darren.chau@cooper.edu>
2023-03-04 21:20:21 -0500

    Update game_round.sql

--- sql/game_round.sql
+++ sql/game_round.sql
@@ -10,10 +10,10 @@ CREATE TABLE game_round (
     --p2_choice varchar(50) NOT NULL,
     
     player_action_amount int NOT NULL DEFAULT 0,  --Amount the player transfers or attacks with
-    p1_hand1 int NOT NULL DEFAULT 5,    
-    p1_hand2 int NOT NULL DEFAULT 5,    
-    p2_hand1 int NOT NULL DEFAULT 5,
-    p2_hand2 int NOT NULL DEFAULT 5,
+    p1_hand1 int NOT NULL DEFAULT 1,    
+    p1_hand2 int NOT NULL DEFAULT 1,    
+    p2_hand1 int NOT NULL DEFAULT 1,
+    p2_hand2 int NOT NULL DEFAULT 1,
     
     PRIMARY KEY (game_id, round_number),
     FOREIGN KEY (game_id) REFERENCES game(game_id) ON DELETE CASCADE
