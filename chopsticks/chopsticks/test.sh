53594a0188eeaa2ef5d76ceefbca7e43f0bbf6d4
map32 <shin3@cooper.edu>
2023-03-05 19:11:38 -0500

    shell script for curl api calls

---
+++ chopsticks/chopsticks/test.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d "{\"gameId\":\"10\",\"playerTurn\":\"Jang\",\"playerChoice\":\"Attack\",\"amount\":\"2\",\"playerHandUsed\":\"p2Hand1\",\"target\":\"p1Hand1\",\"p1Hand1\":\"3\",\"p1Hand2\":\"1\",\"p2Hand1\":\"2\",\"p2Hand2\":\"1\",\"roundNumber\":\"2\"}" http://localhost:8080/insertGameRound
