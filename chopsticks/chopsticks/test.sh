66014361e099c254894fe5142ed5fe0102a229f3
Darren Chau <darren.chau@cooper.edu>
2023-03-05 22:50:39 -0500

    Update test.sh

--- chopsticks/chopsticks/test.sh
+++ chopsticks/chopsticks/test.sh
@@ -1,16 +1,16 @@
-curl -X POST -H "Content-Type: application/json" -d "{\"userName\":\"testing\",\"password\":\"apassword\"}" http://localhost:8080/insertPlayer
+curl -X POST -H "Content-Type: application/json" -d "{\"userName\":\"testing\",\"password\":\"apassword\"}" http://localhost:8080/insertPlayer 
 
 curl -X POST -H "Content-Type: application/json" -d "{\"userName\":\"playerOne\",\"password\":\"passwordOne\"}" http://localhost:8080/insertPlayer
 
 curl -X POST -H "Content-Type: application/json" -d "{\"userName\":\"playerTwo\",\"password\":\"passwordTwo\"}" http://localhost:8080/insertPlayer
 
-curl -X POST "http://localhost:8080/getPlayerById" -H "Content-Type: application/json" -d "1"
+curl -X POST -H "Content-Type: application/json" -d "{\"id\":\"2\"}" http://localhost:8080/getPlayerById
 
-curl -X POST "http://localhost:8080/getPlayerById" -H "Content-Type: application/json" -d "2"
+curl -X POST -H "Content-Type: application/json" -d "{\"id\":\"3\"}" http://localhost:8080/getPlayerById
 
-curl -X POST -H "Content-Type: application/json" -d "{\"p1Id\":\"1\",\"p2Id\":\"2\"}" http://localhost:8080/insertGame
+curl -X POST -H "Content-Type: application/json" -d "{\"p1Id\":\"2\",\"p2Id\":\"3\"}" http://localhost:8080/insertGame
 
-curl -X POST "http://localhost:8080/getGameById" -H "Content-Type: application/json" -d "1"
+curl -X POST -H "Content-Type: application/json" -d "{\"id\":\"1\"}" http://localhost:8080/getGameById
 
 curl -X POST -H "Content-Type: application/json" -d "{\"gameId\":\"1\",\"playerTurn\":\"playerOne\",\"playerChoice\":\"Attack\",\"amount\":\"1\",\"playerHandUsed\":\"p1Hand1\",\"target\":\"p2Hand1\",\"p1Hand1\":\"1\",\"p1Hand2\":\"1\",\"p2Hand1\":\"1\",\"p2Hand2\":\"1\",\"roundNumber\":\"1\"}" http://localhost:8080/insertGameRound
 
