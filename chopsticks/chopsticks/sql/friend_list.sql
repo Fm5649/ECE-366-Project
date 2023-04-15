e67608061558a08abc0dffc93f594b011ab74af9
Darren Chau <darren.chau@cooper.edu>
2023-04-14 21:33:05 -0400

    Update friend_list.sql

--- chopsticks/chopsticks/sql/friend_list.sql
+++ chopsticks/chopsticks/sql/friend_list.sql
@@ -1,1 +1,9 @@
+--sql file for friendlist entries
 
+\c chopsticks
+
+CREATE TABLE friend_list(
+  friendlist_username varchar(30) NOT NULL,
+  friendlist_player_id bigint NOT NULL,
+  FOREIGN KEY (friendlist_player_id) REFERENCES player(player_id) ON DELETE CASCADE
+);
