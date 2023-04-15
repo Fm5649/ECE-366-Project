81e976bacc2f32c086644e359fe2bf27158154c0
Darren Chau <darren.chau@cooper.edu>
2023-04-14 21:46:16 -0400

    Update FriendListDAO.java

--- chopsticks/chopsticks/src/main/java/com/teamchop/chopsticks/FriendListDAO.java
+++ chopsticks/chopsticks/src/main/java/com/teamchop/chopsticks/FriendListDAO.java
@@ -1,1 +1,16 @@
+package com.teamchop.chopsticks;
 
+import com.teamchop.chopsticks.util.DataAccessObject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class FriendListDAO extends DataAccessObject{
+
+    public FriendListDAO(Connection connection) {
+        super(connection);
+    }
+  
+  //INSERT CODE HERE
+}
