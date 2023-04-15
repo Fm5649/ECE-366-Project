57044fbaf7a681b55efab523bf094f4e4b602286
Darren Chau <darren.chau@cooper.edu>
2023-04-14 21:38:23 -0400

    Update FriendList.java

--- chopsticks/chopsticks/src/main/java/com/teamchop/chopsticks/FriendList.java
+++ chopsticks/chopsticks/src/main/java/com/teamchop/chopsticks/FriendList.java
@@ -1,1 +1,32 @@
+package com.teamchop.chopsticks;
 
+import com.teamchop.chopsticks.util.DataTransferObject;
+
+public class FriendList implements DataTransferObject {
+  private long userId;
+  private String username;
+  
+  public long getId() {
+      return userId;
+  }
+
+  public long getUserId() {
+      return userId;
+  }
+  
+  public String getUsername() {
+      return username;
+  }
+
+  public void setUsername(String username) {
+      this.username = username;
+  }
+  
+  @Override
+    public String toString() {
+        return "FriendList{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
