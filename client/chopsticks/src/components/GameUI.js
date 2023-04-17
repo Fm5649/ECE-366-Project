af5e4ce6ca1222b0a7c699436e6bbbafbacc43d8
map32 <shin3@cooper.edu>
2023-04-17 16:22:32 -0400

    game name

--- client/chopsticks/src/components/GameUI.js
+++ client/chopsticks/src/components/GameUI.js
@@ -3,9 +3,6 @@ import { useParams } from 'react-router-dom';
 import SockJsClient from "react-stomp";
 import { useNavigate } from 'react-router-dom';
 import { useRef, useEffect} from "react";
-import axios from "axios";
-
-import { useNavigate } from 'react-router-dom';
 import { auth } from '..';
 import { database } from '..';
 import axios from "axios";
---
+++ client/chopsticks/src/components/Leaderboards.js
@@ -0,0 +1,69 @@
+import '../styles/Leaderboards.css';
+import React, { useState } from 'react';
+import { useRef, useEffect} from "react";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+
+function Leaderboards() {
+    const [connected,setConnected] = useState(false);
+    const [players, setPlayers] = useState([]);
+    const [playerInfo, setPlayerInfo] = useState({
+    playerId: null,
+    playerName: null,
+    playerElo: null
+    }); 
+
+    const navigate = useNavigate();
+    const clientRef = useRef();
+    const joinHandler = () => {
+        console.log(clientRef.current);
+        setConnected(true);
+    }
+    console.log('poo');
+    console.log(players);
+
+    const f = async () => {
+        try {
+            const res = await axios.get(`http://localhost:8080/getPlayers`);
+            const players = res.data.map(player=> ({
+                playerId: player.playerId,
+                playerName: player.playerName,
+                playerElo: player.playerElo
+            }));
+            console.log(players);
+            setPlayers(players);
+        } catch (error) {
+            console.log(error);
+        }
+    } 
+
+    useEffect(()=>{}, [players]);
+
+    if (players.length === 0) {
+        console.log('poo2');
+        f();
+        console.log(players);
+    }
+
+    const sortedPlayers = [...players].sort((a, b) => b.playerElo - a.playerElo);
+
+    return (
+        <div className="leaderboard">
+            <div className="leaderboard-header">
+            <div className="leaderboard-rank">#</div>
+            <div className="leaderboard-player">Player</div>
+            <div className="leaderboard-elo">Elo</div>
+            </div>
+        {sortedPlayers.map((player, index) => (
+            <div className="leaderboard-row" key={player.playerId}>
+            <div className="leaderboard-rank">{index + 1}</div>
+            <div className="leaderboard-player">{player.playerName}</div>
+            <div className="leaderboard-elo">{player.playerElo}</div>
+            </div>
+        ))}
+        </div>
+    );
+}
+
+export default Leaderboards;
