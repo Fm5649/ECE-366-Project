e0f1f0f72286a767bfe895f6293f56e61f6f51f4
Darren Chau <darren.chau@cooper.edu>
2023-05-07 02:14:49 -0400

    Update GameRoundTest.java

--- chopsticks/chopsticks/src/test/java/GameRoundTest.java
+++ chopsticks/chopsticks/src/test/java/GameRoundTest.java
@@ -22,11 +22,59 @@ public class GameRoundTest {
   gameRound.setP2Hand2(4);
   
   @Test
-    @DisplayName("Create Player successfully")
-    public void testWithdraw() {
-        Player player = PlayerDAO.insertUserName("testing@cooper.edu" , "testing", "testingPassword") 
-      
-        assertEquals(200, bankAccount.getBalance());
+    @DisplayName("Get ID successfully")
+    public void testGetGameId() {
+        assertEquals(15, game.getGameId());
+    }
+    @Test
+    @DisplayName("Get round number successfully")
+    public void testGetRoundNumber() {
+        assertEquals(4, game.getRoundNumber());
+    }
+    @Test
+    @DisplayName("Get player turn successfully")
+    public void testGetPlayerTurn() {
+        assertEquals("player1", game.getPlayerTurn());
+    }
+    @Test
+    @DisplayName("Get player choice successfully")
+    public void testGetPlayerChoice() {
+        assertEquals("Attack", game.getPlayerChoice());
+    }
+    @Test
+    @DisplayName("Get player hand used successfully")
+    public void testGetPlayerHandUsed() {
+        assertEquals("left", game.getPlayerHandUsed());
+    }
+    @Test
+    @DisplayName("Get target successfully")
+    public void testGetTarget() {
+        assertEquals("right", game.getTarget());
+    }
+    @Test
+    @DisplayName("Get action amount successfully")
+    public void testGetAmount() {
+        assertEquals(3, game.getAmount());
+    }
+    @Test
+    @DisplayName("Get p1 hand 1 successfully")
+    public void testGetP1Hand1() {
+        assertEquals(3, game.getP1Hand1());
+    }
+    @Test
+    @DisplayName("Get p1 hand 2 successfully")
+    public void testGetP1Hand2() {
+        assertEquals(2, game.getP1Hand2());
+    }
+    @Test
+    @DisplayName("Get p2 hand 1 successfully")
+    public void testGetP2Hand1() {
+        assertEquals(1, game.getP2Hand1());
+    }
+    @Test
+    @DisplayName("Get p2 hand 2 successfully")
+    public void testGetP2Hand2() {
+        assertEquals(4, game.getP2Hand2());
     }
 }
 
