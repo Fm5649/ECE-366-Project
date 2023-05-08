df8f42d551cad93157066fc8b7b9603a0ea9d544
Darren Chau <darren.chau@cooper.edu>
2023-05-08 19:05:47 -0400

    Create ControllersTest.java

---
+++ chopsticks/chopsticks/src/test/java/ControllersTest.java
@@ -0,0 +1,41 @@
+import com.teamchop.chopsticks.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Test Controller class")
+public class ControllerTest{
+  @Mock
+    Controllers controllers;
+  
+  @BeforeEach
+    public void setupMocks() {
+        Mockito.when(controllers.getPlayers()).thenReturn("player");
+        Mockito.when(controllers.getPla
+      
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+
