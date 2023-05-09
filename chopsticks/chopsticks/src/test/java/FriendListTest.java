01be75347532f1e116e1120d9cd64bf52aa484e2
Darren Chau <darren.chau@cooper.edu>
2023-05-09 03:16:37 -0400

    Create FriendListTest.java

---
+++ chopsticks/chopsticks/src/test/java/FriendListTest.java
@@ -0,0 +1,39 @@
+import com.teamchop.chopsticks.FriendList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DisplayName("Test Friend List class")
+public class FriendListTest {
+  private FriendList friendList;
+
+  @BeforeEach
+  void setup() {
+    friendList = new FriendList();
+    friendList.setUserId(50);
+    friendList.setUsername("person");
+    friendList.setIdToken("player1");
+  }
+  @Test
+    @DisplayName("Get ID successfully")
+    public void testGetUserId() {
+        assertEquals(50, friendList.getUserId());
+    }
+  @Test
+    @DisplayName("Get username successfully")
+    public void testGetUserName() {
+        assertEquals("person", friendList.getUsername());
+    }
+  @Test
+    @DisplayName("Get IdToken successfully")
+    public void testGetIdToken() {
+        assertEquals("player1", friendList.getIdToken());
+    }
+}
